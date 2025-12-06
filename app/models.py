1b35be2b568713c7e00774248b01bf4a1ec2f22b
likanug <likanug.g@qq.com>
2025-12-06 17:21:32 +0800

    feat: models新增news_item

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Table, Column, BigInteger, String, Date, JSON, TIMESTAMP, MetaData, UniqueConstraint
+from sqlalchemy import Table, Column, BigInteger, String, Date, Text, TIMESTAMP, MetaData, ForeignKey, JSON, \
+    UniqueConstraint
+from sqlalchemy.sql import func
 
 metadata = MetaData()
 
@@ -13,4 +15,18 @@ news_info = Table(
     Column("created_at", TIMESTAMP(timezone=True)),
     Column("updated_at", TIMESTAMP(timezone=True)),
     UniqueConstraint("news_from", "news_date", name="uniq_news_info")
+)
+
+news_item = Table(
+    "news_item",
+    metadata,
+    Column("id", Text, primary_key=True),  # 使用Text对应PostgreSQL的text类型
+    Column("news_info_id", BigInteger, ForeignKey("news_info.id", ondelete="CASCADE")),
+    Column("title", Text, nullable=False),
+    Column("url", Text, nullable=False),
+    Column("published_at", Date),
+    Column("source", String(50)),
+    Column("content", Text),
+    Column("created_at", TIMESTAMP(timezone=True), server_default=func.current_timestamp()),
+    Column("updated_at", TIMESTAMP(timezone=True), server_default=func.current_timestamp()),
 )
