3f6d491b571c385d55446c67840c3f8ae640a171
likanug <likanug.g@qq.com>
2025-12-13 23:17:26 +0800

    feat: 移除分词模型加载的相关代码

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-import logging
 import os
-from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import RedirectResponse
-from wordfreq_cn.core import get_tokenizer
 
 from app import settings
 from app.routers import analysis, search, news
 
-# 获取日志器
-logger = logging.getLogger(__name__)
-
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    # 服务启动时执行
-    logger.info("检查并预取分词模型...")
-    get_tokenizer() # 此时由于 Docker 已经下载好了，这里只是从磁盘加载到内存
-    yield
-
-app = FastAPI(title="News Analytics API", lifespan=lifespan)
+app = FastAPI(title="News Analytics API")
 
 # 创建静态文件夹
 os.makedirs(settings.WORDCLOUD_DIR, exist_ok=True)
